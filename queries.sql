4391e8f30f29727857c2fd294e8528d19195ce2b
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-03-23 13:36:11 -0600

    add species column to one of the output queries

--- queries.sql
+++ queries.sql
@@ -42,5 +42,5 @@ SELECT COUNT(name) FROM animals;
 SELECT COUNT(name) FROM animals WHERE escape_attempts = 0;
 SELECT AVG(weight_kg) FROM animals;
 SELECT neutered, SUM(escape_attempts) FROM animals GROUP BY neutered;
-SELECT MAX(weight_kg), MIN(weight_kg) FROM animals GROUP BY species;
+SELECT species, MAX(weight_kg), MIN(weight_kg) FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth >= '1990-01-01' AND date_of_birth < '2000-01-01' GROUP BY species;
