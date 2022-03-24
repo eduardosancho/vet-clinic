130df4ded310069b732ada720d94253d2aa5411a
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-03-23 22:21:32 -0600

    Add items to species table

--- data.sql
+++ data.sql
@@ -18,3 +18,6 @@ INSERT INTO owners (full_name, age) VALUES ('Bob', 45);
 INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
 INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
 INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
+
+INSERT INTO species (name) VALUES ('Pokemon');
+INSERT INTO species (name) VALUES ('Digimon');
