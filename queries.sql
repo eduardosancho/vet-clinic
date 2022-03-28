fd353f48fcd8844222537fade7171d78290e1fc1
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-03-29 00:58:07 +0300

    Measure the execution time

--- queries.sql
+++ queries.sql
@@ -135,3 +135,29 @@ INNER JOIN animals ON animals.id = visits.animals_id
 INNER JOIN species ON species.id = animals.species_id
 WHERE vets.name = 'Maisy Smith'
 GROUP BY species.name LIMIT 1;
+
+EXPLAIN ANALYZE
+SELECT
+  COUNT(*)
+FROM
+  visits
+WHERE
+  animal_id = 4;
+
+EXPLAIN ANALYZE
+SELECT
+  *
+FROM
+  visits
+WHERE
+  vet_id = 2;
+
+EXPLAIN ANALYZE
+SELECT
+  *
+FROM
+  owners
+WHERE
+  email = 'owner_18327@mail.com';
+  
+   
