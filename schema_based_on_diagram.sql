c91ef33eca97ce95a36e28b43b6ced8185153a19
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-03-29 14:36:51 -0600

    Re arrange schema, to avoid reference errors

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,38 +1,44 @@
 CREATE TABLE patients (
-    id int GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) NOT NULL PRIMARY KEY,
-    name varchar(50),
-    date_of_birth date,
+  id int GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) NOT NULL PRIMARY KEY,
+  name varchar(50),
+  date_of_birth date
 );
 
 CREATE TABLE invoices (
     id int GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) NOT NULL PRIMARY KEY,
     total_amount decimal,
     generated_at timestamp,
-    payed_at timestamp,
-    medical_history_id int REFERENCES medical_histories (id) ON UPDATE CASCADE,
+    payed_at timestamp
 );
 
 CREATE TABLE invoice_items (
     id int GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) NOT NULL PRIMARY KEY,
     unit_price decimal,
     quantity int,
-    total_price decimal,
-    invoice_id int REFERENCES invoices (id) ON UPDATE CASCADE,
-    treatment_id int REFERENCES treatments (id) ON UPDATE CASCADE,
+    total_price decimal
 );
 
 CREATE TABLE medical_histories (
     id int GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) NOT NULL PRIMARY KEY,
     admitted_at timestamp,
-    status varchar(50),
-    patient_id int REFERENCES patients (id) ON UPDATE CASCADE,
+    status varchar(50)
 );
 
 CREATE TABLE treatments (
   id int GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) NOT NULL PRIMARY KEY,
   type varchar(50),
-  name varchar(50),
+  name varchar(50)
 );
+
+ALTER TABLE invoices
+  ADD medical_history_id int REFERENCES medical_histories (id) ON UPDATE CASCADE;
+
+ALTER TABLE invoice_items 
+  ADD invoice_id int REFERENCES invoices (id) ON UPDATE CASCADE,
+  ADD treatment_id int REFERENCES treatments (id) ON UPDATE CASCADE;
+
+ALTER TABLE medical_histories
+  ADD patient_id int REFERENCES patients (id) ON UPDATE CASCADE;
 
 CREATE TABLE treatment_histories (
   medical_histories_id int REFERENCES medical_histories (id) ON UPDATE CASCADE,
