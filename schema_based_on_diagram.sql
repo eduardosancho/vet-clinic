1483dcf641b5b5886ec6d79941d3651088108841
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-03-29 22:40:25 +0300

    Add patients, medical_histories, and invoices tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -9,6 +9,13 @@ CREATE TABLE invoices (
     total_amount decimal,
     generated_at timestamp,
     payed_at timestamp,
-    medical_history_id int,
+    medical_history_id int REFERENCES medical_histories (id) ON UPDATE CASCADE,
+);
+
+CREATE TABLE medical_histories (
+    id int GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) NOT NULL PRIMARY KEY,
+    admitted_at timestamp,
+    status varchar(50),
+    patient_id int REFERENCES patients (id) ON UPDATE CASCADE,
 );
 
